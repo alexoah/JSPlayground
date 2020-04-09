1e6d84834aaa644b5cfa7e640e763bbe33852c6b
Alexoah YT <alexandra.soah@gmail.com>
2020-04-09 17:51:03 +0700

    added answer files for JavaScript Data Types exercises

---
+++ W3School-JSExercises/JS-DataTypes/jsDataTypesE1.js
@@ -0,0 +1,20 @@
+/**
+ *	from JavaScript Data Types: Exercise 1 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_datatypes1 )
+ *
+ *	question:
+ *	Use comments to describe the correct data type of the following variables:
+ *
+ *	var length = 16;          // ______
+ *	var lastName = "Johnson"; // ______
+ *	var x = {
+ *		firstName: "John",
+ *		lastName: "Doe"
+ *	};                        // ______
+ *
+ */
+var length = 16;          // Number
+var lastName = "Johnson"; // String
+var x = {
+	firstName: "John",
+	lastName: "Doe"
+};                        // Object
