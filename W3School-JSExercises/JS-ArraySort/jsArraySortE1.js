19a1b1f62147e39b2e161327829293d6250b09c1
Alexoah YT <alexandra.soah@gmail.com>
2020-04-09 18:32:28 +0700

    added answer files for JavaScript Array Sort exercises

---
+++ W3School-JSExercises/JS-ArraySort/jsArraySortE1.js
@@ -0,0 +1,12 @@
+/**
+ *	from JavaScript Array Sort: Exercise 1 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_array_sort1 )
+ *
+ *	question:
+ *	Use the correct Array method to sort the fruits array alphabetically.
+ *
+ *	var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+ *	_____________;
+ *
+ */
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.sort();
