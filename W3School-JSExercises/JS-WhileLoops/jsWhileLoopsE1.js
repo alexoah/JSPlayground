1875893cd834b60a3d85642aa3312ff44eb61776
Alexoah YT <alexandra.soah@gmail.com>
2020-04-11 13:34:11 +0700

    added answer files for JavaScript While Loops exercises

---
+++ W3School-JSExercises/JS-WhileLoops/jsWhileLoopsE1.js
@@ -0,0 +1,18 @@
+/**
+ *	from JavaScript While Loops: Exercise 1 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_loop_while1 )
+ *
+ *	question:
+ *	Create a loop that runs as long as i is less than 10.
+ *
+ *	var i = 0;
+ *	_____ (i _ 10) {
+ *		console.log(i);
+ *		i++
+ *	}
+ *
+ */
+var i = 0;
+while (i < 10) {
+	console.log(i);
+	i++
+}
---
+++ W3School-JSExercises/JS-WhileLoops/jsWhileLoopsE2.js
@@ -0,0 +1,18 @@
+/**
+ *	from JavaScript While Loops: Exercise 2 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_loop_while2 )
+ *
+ *	question:
+ *	Create a loop that runs as long as i is less than 10, but increase i with 2 each time.
+ *
+ *	var i = 0;
+ *	while (i < 10) {
+ *		console.log(i);
+ *		_ = _ _ _;
+ *	}
+ *
+ */
+var i = 0;
+while (i < 10) {
+	console.log(i);
+	i = i + 2;
+}
