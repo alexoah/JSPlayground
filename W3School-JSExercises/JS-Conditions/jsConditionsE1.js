b93ff43a18ebb9783f7be79a894b12842989535f
Alexoah YT <alexandra.soah@gmail.com>
2020-04-11 10:21:56 +0700

    added answer files for JavaScript Conditions exercises

---
+++ W3School-JSExercises/JS-Conditions/jsConditionsE1.js
@@ -0,0 +1,14 @@
+/**
+ *	from JavaScript Conditions: Exercise 1 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_conditions1 )
+ *
+ *	question:
+ *	Fix the if statement to alert "Hello World" if x is greater than y.
+ *
+ *	if _x > y_ _
+ *		alert("Hello World");
+ *	_
+ *
+ */
+if (x > y) {
+	alert("Hello World");
+}
---
+++ W3School-JSExercises/JS-Conditions/jsConditionsE2.js
@@ -0,0 +1,18 @@
+/**
+ *	from JavaScript Conditions: Exercise 2 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_conditions2 )
+ *
+ *	question:
+ *	Fix the if statement to alert "Hello World" if x is greater than y, otherwise alert "Goodbye".
+ *
+ *	__ (x _ y) {
+ *		alert("Hello World");
+ *	} ____ {
+ *		alert("Goodbye");
+ *	}
+ *
+ */
+if (x > y) {
+	alert("Hello World");
+} else {
+	alert("Goodbye");
+}
