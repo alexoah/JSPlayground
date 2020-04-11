6916226c206722915f6654a6accacc7cfee576a5
Alexoah YT <alexandra.soah@gmail.com>
2020-04-11 10:22:18 +0700

    added answer files for JavaScript Switch exercises

---
+++ W3School-JSExercises/JS-Switch/jsSwitchE1.js
@@ -0,0 +1,24 @@
+/**
+ *	from JavaScript Switch: Exercise 1 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_switch1 )
+ *
+ *	question:
+ *	Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
+ *
+ *	______(fruits) {
+ *		____ "Banana":
+ *			alert("Hello")
+ *			break;
+ *		____ "Apple":
+ *			alert("Welcome")
+ *			break;
+ *	}
+ *
+ */
+switch(fruits) {
+	case "Banana":
+		alert("Hello")
+		break;
+	case "Apple":
+		alert("Welcome")
+		break;
+}
---
+++ W3School-JSExercises/JS-Switch/jsSwitchE2.js
@@ -0,0 +1,28 @@
+/**
+ *	from JavaScript Switch: Exercise 2 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_switch2 )
+ *
+ *	question:
+ *	Add a section that will alert("Neither") if fruits is neither "banana" nor "apple".
+ *
+ *	switch(fruits) {
+ *		case "Banana":
+ *			alert("Hello")
+ *			break;
+ *		case "Apple":
+ *			alert("Welcome")
+ *			break;
+ *		________
+ *			alert("Neither");
+ *	}
+ *
+ */
+switch(fruits) {
+	case "Banana":
+		alert("Hello")
+		break;
+	case "Apple":
+		alert("Welcome")
+		break;
+	default:
+		alert("Neither");
+}
