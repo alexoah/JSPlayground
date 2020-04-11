0639d82e5a79b595c0e342b9302ce2b6fcf3aa8c
Alexoah YT <alexandra.soah@gmail.com>
2020-04-11 13:33:46 +0700

    added answer files for JavaScript For Loops exercises

---
+++ W3School-JSExercises/JS-ForLoops/jsForLoopsE1.js
@@ -0,0 +1,16 @@
+/**
+ *	from JavaScript For Loops: Exercise 1 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_loops1 )
+ *
+ *	question:
+ *	Create a loop that runs from 0 to 9.
+ *
+ *	var i;
+ *	___ (_ = _; _ < __; ___) {
+ *		console.log(i);
+ *	}
+ *
+ */
+var i;
+for (i = 0; i < 10; i++) {
+	console.log(i);
+}
---
+++ W3School-JSExercises/JS-ForLoops/jsForLoopsE2.js
@@ -0,0 +1,16 @@
+/**
+ *	from JavaScript For Loops: Exercise 2 ( https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_loops2 )
+ *
+ *	question:
+ *	Create a loop that runs through each item in the fruits array.
+ *
+ *	var fruits = ["Apple", "Banana", "Orange"];
+ *	for (x __ ______) {
+ *		console.log(x);
+ *	}
+ *
+ */
+var fruits = ["Apple", "Banana", "Orange"];
+for (x in fruits) {
+	console.log(x);
+}
